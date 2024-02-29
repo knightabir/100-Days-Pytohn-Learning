2f93ba45168cddbe961caef0f10d2399642a52f0
Abir Sarkar <abirsarkar1999@gmail.com>
2024-03-01 01:07:18 +0530

    Day 3

---
+++ Day_3/module and pip.py
@@ -0,0 +1,22 @@
+# # Modules is like a code which can be used to borrow the code written by someone else in our python program. There
+# are two types of modules in python
+
+# 1) Build in Module : These modules are ready to import and use and ships with the python interrupter. There is no
+# need to install such module explicitly
+
+# 2) External Module : These modules are imported from a third party  file or can be installed using a package
+# manager like pip or conda. Since this code is written by someone else, we can install different version of a same
+# module with time.
+
+
+# The pip command it can be used as a package manager pip to install a python module. Let's install a module called
+# pandas using the following command
+
+# pip install pandas
+
+import pandas
+
+pandas.read_csv('words.csv')
+
+# Similarly we can install other module and look into their documentation for use instruction.
+# We will find ourselves doing this often in the later part of this course
