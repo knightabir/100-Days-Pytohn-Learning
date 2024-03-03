973b1208a5f54cdcde25a84416e50ccd8bb9c4cc
Abir Sarkar <abirsarkar1999@gmail.com>
2024-03-04 01:23:08 +0530

    Day 6 complete...

---
+++ Day_6/main.py
@@ -0,0 +1,17 @@
+# Variables and datatypes
+
+a = "Abir Sarkar"
+
+
+def func(string):
+    print("Hello", string)
+
+
+# func(a)
+
+b = 123
+c = "Abir Sarkar"
+
+print(b + b)
+
+print(type(a), type(b), type(c))
